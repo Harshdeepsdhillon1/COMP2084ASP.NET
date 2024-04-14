2b3ef6ac32de6dcd046941abc459db5432c093f0
Harshdeepsdhillon1 <harshdeepsdhillon1@gmail.com>
2024-04-14 02:52:56 -0400

    Lab 4 xUnit Testing and APIs

---
+++ Lab 4/LabWebApp/LabWebAppUI/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LabWebAppUI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+   
+
+        public string Description { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public decimal Price { get; set; } // Added [DisplayFormat] attribute to format as currency
+    }
+}
