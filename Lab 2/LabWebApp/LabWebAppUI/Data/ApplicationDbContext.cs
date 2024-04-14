167f380de1226779823c91262925214e5734d9a0
Harshdeepsdhillon1 <harshdeepsdhillon1@gmail.com>
2024-04-13 20:42:32 -0400

    Updated Lab 2
    added new code and connection strings for lab 2

---
+++ Lab 2/LabWebApp/LabWebAppUI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LabWebAppUI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LabWebAppUI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; } // Add DbSet<Product> property
+    }
+}
